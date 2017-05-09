650cb4c7067a99ec6b6c34504fedd59f677a8612
namazu <namazu510@hotmail.co.jp>
2017-05-09 15:33:49 +0900

    Fix rmi path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/java/default/bin/rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false
+nohup /usr/bin/rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false &
 
 JAVA_OPTS=${JAVA_OPTS:-"-Xmx128m"}
 exec java $JAVA_OPTS -jar $@
